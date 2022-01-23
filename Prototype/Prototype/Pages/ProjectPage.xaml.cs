fc788fc960fe18c335e9bd4a70fc50c9bccae4e6
sofahmm <74815523+sofahmm@users.noreply.github.com>
2022-01-23 14:48:59 +0300

    Add project files.

---
+++ Prototype/Prototype/Pages/ProjectPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProjectPage : ContentPage
+    {
+        public string[] Projects { get; set; } 
+        public ProjectPage()
+        {
+            InitializeComponent();
+            Projects = new string[] {"Проект 1", "Проект 2", "Проект 3", "Проект 4", 
+                                    "Проект 5", "Проект 6", "Проект 7", "Проект 8", 
+                                    "Проект 9", "Проект 10", "Проект 11", "Проект 12", 
+                                    "Проект 13", "Проект 14", "Проект 15", "Проект 16", 
+                                    "Проект 17", "Проект 18", "Проект 19", };
+            this.BindingContext = this;
+        }
+    }
+}
---
+++ Prototype/Prototype/Pages/ProjectsInfoPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProjectsInfoPage : ContentPage
+    {
+        public ProjectsInfoPage()
+        {
+            InitializeComponent();
+            this.Content = lbl_description;
+        }
+        Label lbl_description = new Label
+        {
+            Text = "Spoken words were meant to last, \n" +
+
+                                                    "but the picture faded fast \n" +
+                                                    "The words I love you meant so much,\n" +
+                                                    "now not even your lips can I touch \n" +
+                                                    "I'll cry for another eyesore and think about our first kiss by the tree \n" +
+                                                    "And how it meant so much to me \n" +
+                                                    "Nobody knows the me that you do \n" +
+                                                    "Gonna take some getting used to \n" +
+                                                    "And I think I'm getting through to you \n" +
+                                                    "But I got shit I put you through \n" +
+                                                    "It ain't right and I know that I'm barely ever home \n" +
+                                                    "But I think of you on the road \n" +
+                                                    "You know I think of you on blow \n" +
+                                                    "It's me, Lil Jeep, she don't keep not a memory of me \n" +
+                                                    "I'ma creep, I'ma freak,\n",  
+            FontSize= 18
+        };
+    }
+}
